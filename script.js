5e7a249970efd46427c05d4e83420a419b67d598
LataBansavade <139533645+LataBansavade@users.noreply.github.com>
2023-08-28 13:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+ async function delayedMessage(message, delay) {
+      await new Promise(resolve => setTimeout(resolve, delay));
+      return message;
+    }
+
+    // Event listener for the button click
+    document.getElementById('btn').addEventListener('click', async () => {
+      const inputText = document.getElementById('text').value;
+      const inputDelay = parseInt(document.getElementById('delay').value);
+
+      if (inputText && inputDelay) {
+        const result = await delayedMessage(inputText, inputDelay);
+
+        const outputDiv = document.getElementById('output');
+        outputDiv.textContent = result;
+      }
+    });
